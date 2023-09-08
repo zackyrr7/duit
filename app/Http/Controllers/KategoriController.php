a58743590630e04eb87daf55a827ec4ef36ccc1f
zackyrr7 <zackyrr7@gmail.com>
2023-09-08 15:17:02 +0700

    user & kategori ||9-8

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\kategori;
+use Exception;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    public function index()
+    {
+        return kategori::all();
+    }
+
+    public function indexUser($id)
+    {
+
+        return kategori::where('users_id', $id)->get();
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $kategori = new kategori();
+            $kategori->users_id = $request->users_id;
+            $kategori->nama = $request->nama;
+            $kategori->icon = $request->icon;
+            $kategori->save();
+
+            return response()->json([
+                'status' => "200",
+                'message' => 'Kategori Berhasil ditambahkan'
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => "500",
+                'message' => "Something went really wrong"
+            ]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $kategori = kategori::find($id);
+            if (!$kategori) {
+                return response()->json([
+                    'status' => '404',
+                    'message' => 'Kategori tidak ditemukan'
+                ]);
+            }
+            $kategori->nama = $request->nama;
+            $kategori->icon = $request->icon;
+            $kategori->users_id = $request->users_id;
+            return response()->json([
+                'status' => '200',
+                'message' => 'kategori berhasil di update'
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => '500',
+                'message' => 'Something went really wrong'
+            ]);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $kategori = kategori::find($id);
+            if (!$kategori) {
+                return response()->json([
+                    'status' => '404',
+                    'message' => 'Kategori tidak ditemukan'
+                ]);
+            }
+            $kategori->delete();
+            return response()->json([
+                'status' => '200',
+                'message' => 'kategori berhasil di hapus'
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => '500',
+                'message' => 'Something went really wrong'
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//auth
+Route::get('/user/all', [AuthController::class, 'index']);
+Route::get('/user/{id}', [AuthController::class, 'indexUser']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/changerole', [AuthController::class, 'changeRole']);
+
+
+//kategori
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori/{id}', [KategoriController::class, 'indexUser']);
+Route::post('/kategori', [KategoriController::class, 'store']);
+Route::post('/kategori/update/{id}', [KategoriController::class, 'update']);
+Route::post('/kategori/delete/{id}', [KategoriController::class, 'destroy']);
